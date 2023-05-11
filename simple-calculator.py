9062013e7d69bc77a87e3f05d9c2d56c834aa269
Bhea <bheaisipnobleza@gmail.com>
2023-05-12 01:24:18 +0800

    pseudocode

---
+++ simple-calculator.py
@@ -0,0 +1,10 @@
+# Ask the user for the operation
+# Ask the user to input two numbers
+    # If addition
+    # If subtraction
+    # If multiplication
+    # If division
+# Ask user if they want to continue in solving
+    # If yes, continue solving
+    # If no, exit the program
+# Call the function
